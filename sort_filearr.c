586f3577d132389fbebe773a9c49c8e7fad8c02d
vgrankul <vgrankul@debian.com>
2020-05-19 15:32:07 +0300

    fixed norme

--- sort_filearr.c
+++ sort_filearr.c
@@ -13,11 +13,19 @@
 #include "./libft/libft.h"
 #include "ft_ls.h"
 
+void	swap_files(t_file **arr, int i, int j)
+{
+	t_file *tmp;
+
+	tmp = arr[i];
+	arr[i] = arr[j + 1];
+	arr[j + 1] = tmp;
+}
+
 void	sort_arr_name(t_file **arr)
 {
 	int		i;
 	int		j;
-	t_file	*tmp;
 	char	*str1;
 	char	*str2;
 
@@ -28,15 +36,12 @@ void	sort_arr_name(t_file **arr)
 		j = i;
 		while (arr[j + 1] != NULL)
 		{
-			if (!(str1 = ft_string_tolower(remove_not_alpha(arr[i]->filename))) ||
+			if (!(str1 = ft_string_tolower(remove_not_alpha(arr[i]->filename)))
+			||
 			!(str2 = ft_string_tolower(remove_not_alpha(arr[j + 1]->filename))))
 				print_error(errno);
 			if (ft_strcmp(str1, str2) > 0)
-			{
-				tmp = arr[i];
-				arr[i] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
+				swap_files(arr, i, j);
 			free(str1);
 			free(str2);
 			j++;
@@ -49,7 +54,6 @@ void	sort_mod_arr_name(t_file **arr)
 {
 	int		i;
 	int		j;
-	t_file	*tmp;
 
 	i = 0;
 	j = 0;
@@ -59,11 +63,7 @@ void	sort_mod_arr_name(t_file **arr)
 		while (arr[j + 1] != NULL)
 		{
 			if (arr[i]->mod_time < arr[j + 1]->mod_time)
-			{
-				tmp = arr[i];
-				arr[i] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
+				swap_files(arr, i, j);
 			j++;
 		}
 		i++;
@@ -74,7 +74,6 @@ void	sort_rev_arr_name(t_file **arr)
 {
 	int		i;
 	int		j;
-	t_file	*tmp;
 
 	i = 0;
 	j = 0;
@@ -83,9 +82,7 @@ void	sort_rev_arr_name(t_file **arr)
 		j = i;
 		while (arr[j + 1] != NULL)
 		{
-			tmp = arr[i];
-			arr[i] = arr[j + 1];
-			arr[j + 1] = tmp;
+			swap_files(arr, i, j);
 			j++;
 		}
 		i++;
